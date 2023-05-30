42ebcca28103c59ed68e7709b4cc3511baaf2d0e
TOUIBISM <samy@linkia.qa>
2023-05-30 10:02:19 +0100

    add load details model

---
+++ lib/app/views/services/pages/loan_requests/LoanRequestDetailsScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoadRequestDetailsScreen extends StatefulWidget {
+  const LoadRequestDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoadRequestDetailsScreen> createState() => _LoadRequestDetailsScreenState();
+}
+
+class _LoadRequestDetailsScreenState extends State<LoadRequestDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
